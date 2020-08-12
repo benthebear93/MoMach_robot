4923bda4f084f45f68e4505d2d08c7aab53826ae
benthebear93 <nswve@naver.com>
2020-08-12 13:48:58 +0900

    chage arduino

--- MoMach_ros/arduino/Zaxis_control/Zaxis_control.ino
+++ MoMach_ros/arduino/Zaxis_control/Zaxis_control.ino
@@ -18,32 +18,30 @@ int g_s_run = 0;
 int g_temp_z = 0;
 int g_theta_z = 0;
 int g_pos_z = 0;
+int g_zaxis_keyboard = 0;
 
 std_msgs::Int16 tool_pos;
 ros::Publisher difference_check("tool_pos_msg", &tool_pos);
 
-void StepperMove(int speed_val)
+void StepperMove(int speed_val) 
 {
   digitalWrite(Pulse, HIGH);
   delayMicroseconds(speed_val);
   digitalWrite(Pulse, LOW);
   delayMicroseconds(speed_val);
-}
+} //motor input
 
-void SwitchStop( const std_msgs::Int16& swt1)
+void ZaxisKeyboard(const std_msgs::Int16& zaxis_input)
 {
-  g_s_switch = swt1.data;
-  if(g_s_switch == 1){
-    nh.loginfo("1.stop");
-  }else{
-    nh.loginfo("0.run");
-  }
-}
+  g_zaxis_keyboard = zaxis_input.data;
+  if(g_zaxis_keyboard==1)nh.loginfo("here");
+  else nh.loginfo("here2");
+} //keyboard interrupt for control z axis with keyboard (while robot running)
 
 void GetControlZ(const MoMach_ros::pos_stream& Controller_data)
 {
   int tool_z = 0; 
-  if(flag_init == 0)
+  if(flag_init == 0) // initialize first z value
   {
     tool_z = Controller_data.pos_z;
     g_temp_z = tool_z;
@@ -56,10 +54,10 @@ void GetControlZ(const MoMach_ros::pos_stream& Controller_data)
     g_pos_z = tool_z;
   }
   ControlMode(g_temp_z, tool_z, 3);
-}
+} // get z axis value from Delta controller
 
 ros::Subscriber<MoMach_ros::pos_stream> sub("pos_stream", GetControlZ);
-ros::Subscriber<std_msgs::Int16> sub2("stop", SwitchStop);
+ros::Subscriber<std_msgs::Int16> sub2("zaxis_key_input", ZaxisKeyboard);
 
 void setup()
 {
@@ -72,34 +70,58 @@ void setup()
 
 void ControlMode(int prev_z, int curr_z, int speed_val)
 {
+  int z_key_input = 0;
   g_theta_z = curr_z - prev_z;
   if(g_theta_z > 0)
   {
-    digitalWrite(Dir, f);
-    for(int i = 0; i < mmResolution*abs(g_theta_z); i++)
+    digitalWrite(Dir, b);
+    for(int i = 0; i < mmResolution*abs(g_theta_z+g_zaxis_keyboard); i++)
     {
       g_pos_z = g_pos_z + 0.2;
+      z_key_input = g_pos_z;
       nh.loginfo("forward");
       StepperMove(speed_val);
     }
   }
   else if(g_theta_z < 0)
   {
-    digitalWrite(Dir, b);
-    for(int i = 0; i < mmResolution*abs(g_theta_z); i++)
+    digitalWrite(Dir, f);
+    for(int i = 0; i < mmResolution*abs(g_theta_z+g_zaxis_keyboard); i++)
     {
       g_pos_z = g_pos_z - 0.2;
+      z_key_input = g_pos_z;
       nh.loginfo("backward");
       StepperMove(speed_val);
     }
   }
   else
   {
+    if(g_zaxis_keyboard > 0)
+    {
+      digitalWrite(Dir, b);
+      for(int i = 0; i < mmResolution*abs(g_zaxis_keyboard); i++)
+      {
+        z_key_input = z_key_input + 0.2;
+        nh.loginfo("keyboard +1");
+        StepperMove(speed_val);
+      }
+    }
+    else if(g_zaxis_keyboard < 0)
+    {
+      digitalWrite(Dir, f);
+      for(int i = 0; i < mmResolution*abs(g_zaxis_keyboard); i++)
+      {
+        z_key_input = z_key_input - 0.2;
+        nh.loginfo("keyboard -1");
+        StepperMove(speed_val);
+      }
+    }
   }
-  tool_pos.data = g_pos_z;
+  tool_pos.data = z_key_input;
   difference_check.publish(&tool_pos);
   g_temp_z = curr_z;
-}
+  g_zaxis_keyboard = 0;
+} // mimic Delta z axis motor motion based on z axis data. 
 
 void loop()
 {
