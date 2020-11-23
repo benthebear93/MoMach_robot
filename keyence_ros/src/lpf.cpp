983f50d3204d469a0bf5d607ff2c670f9d03b055
benthebear93 <nswve@naver.com>
2020-11-23 20:17:26 +0900

    lpf class

--- keyence_ros/src/lpf.cpp
+++ keyence_ros/src/lpf.cpp
@@ -18,17 +18,22 @@ class LPF
         ~LPF();
         bool init();
         void LowPassFilter();
+        void Original();
+        double pre_val_;
+        double curr_val_;
+        double lk_val_;
+        double lj_val_;
     private:
         ros::NodeHandle nh_;
 
-        ros::Publisher lk_dis_pub_;
-        ros::Publisher lj_dis_pub_;
+        //ros::Publisher lk_dis_pub_;
+        //ros::Publisher lj_dis_pub_;
+        ros::Publisher extract_laser_;
 
         ros::Subscriber lk_sub_;
         ros::Subscriber lj_sub_;
 
-        double pre_val_;
-        double curr_val_;
+        double ext_val_;
         double avg_curr_val_;
         double ts_;
         double tau_;
@@ -49,13 +54,16 @@ LPF::~LPF()
 }
 bool LPF::init()
 {
-    pre_val_     = 0;
-    curr_val_     = 0;
-    avg_curr_val_ = 0;
-    ts_           = 0.005;
-    tau_          = 0.001;
-    lk_dis_pub_   = nh_.advertise<std_msgs::Float32>("lf_lk_dis", 10);
-    lj_dis_pub_   = nh_.advertise<std_msgs::Float32>("lf_lj_dis", 10);
+    pre_val_         = 0;
+    curr_val_        = 0;
+    lk_val_          = 0;
+    lj_val_          = 0;
+    ext_val_         = 0;
+    ts_              = 0.005;
+    tau_             = 0.5;
+    //lk_dis_pub_      = nh_.advertise<std_msgs::Float32>("lf_lk_dis", 10);
+    //lj_dis_pub_      = nh_.advertise<std_msgs::Float32>("lf_lj_dis", 10);
+    extract_laser_   = nh_.advertise<std_msgs::Float32>("extract_laser", 10);
 
     lk_sub_  = nh_.subscribe("LK_laser", 10, &LPF::lk_callback, this);
     lj_sub_  = nh_.subscribe("mid_laser", 10, &LPF::lj_callback, this);
@@ -64,40 +72,47 @@ bool LPF::init()
 
 void LPF::lk_callback(const std_msgs::Float32::ConstPtr& msg)
 {
-    std_msgs::Float32 distance;
-    distance.data = pre_val_;
-    lk_dis_pub_.publish(distance);
-    curr_val_ = msg->data;
+    //std_msgs::Float32 distance;
+    //distance.data = pre_val_;
+    //lk_dis_pub_.publish(distance);
+    lk_val_ = msg->data;
 }
 void LPF::lj_callback(const std_msgs::Float32::ConstPtr& msg)
 {
-    std_msgs::Float32 distance;
-    distance.data = pre_val_;
-    lj_dis_pub_.publish(distance);
-    curr_val_ = msg->data;
+    //std_msgs::Float32 distance;
+    //distance.data = pre_val_;
+    //lj_dis_pub_.publish(distance);
+    lj_val_ = msg->data;
 }
 
 void LPF::LowPassFilter()
 {
-    cout << "curr_val_ : " << curr_val_<<endl;
-    cout << "pre_val_ : " << pre_val_ << endl;
+    std_msgs::Float32 surface_laser;
     curr_val_ = (tau_*pre_val_+curr_val_*ts_)/(tau_ + ts_);
     pre_val_ = curr_val_;
+    surface_laser.data = curr_val_;
+    extract_laser_.publish(surface_laser);
 }
+
+void LPF::Original()
+{
+    curr_val_ = lj_val_-lk_val_;
+    cout << "lj : "<< lj_val_ << "lk : "<< lk_val_ << "curr val : " << curr_val_ << endl;
+}
+
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "lpf");
-  LPF lk_lpf;
-  LPF lj_lpf;
+  LPF lpf;
 
   ros::Rate loop_rate(100);
   while(ros::ok())
   {
-    lk_lpf.LowPassFilter();
-    lj_lpf.LowPassFilter();
+    lpf.Original();
+    lpf.LowPassFilter();
     ros::spinOnce();
-    loop_rate.sleep();
+    //loop_rate.sleep();
   }
   return 0;
 }
