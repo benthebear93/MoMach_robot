a815bfe56c34f3c55969894f112aa43fb2e38f79
benthebear93 <nswve@naver.com>
2020-11-26 00:36:22 +0900

    PCL Update

--- keyence_ros/src/keyence_profile.cpp
+++ keyence_ros/src/keyence_profile.cpp
@@ -16,23 +16,47 @@
 #include <pcl/io/pcd_io.h>
 #include <pcl_ros/point_cloud.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <vector>
 //define
 #define KEYENCE "169.254.29.71"
 #define PORT 24691
 using namespace std;
+/*struct XYZCoordinate{
+    double x[16];
+    double y[16];
+    double z[16];
+};*/
 
+typedef pcl::PointCloud<pcl::PointXYZ> Cloud;
+const static std::string DEFAULT_FRAME_ID = "lj_v7080_optical_frame";
+double pos_y = 0;
+double temp = 0;
+
+void pos_callback(const std_msgs::Float32::ConstPtr& msg)
+{
+    pos_y = msg->data;
+    temp = pos_y;
+}
 
 int main(int argc, char**argv)
 {
-    pcl::PointCloud<pcl::PointXYZ> cloud;
-    cloud.width = 16;
-    cloud.height = 1;
-    cloud.is_dense = false;
-    cloud.points.resize (cloud.width * cloud.height);
-    //init pcl format
     std_msgs::Float32 mid_laser;
     ros::init(argc, argv, "keyence_get_profile");
-    ros::NodeHandle nh;
+    ros::NodeHandle nh, pnh("~");
+
+    std::string frame_id;
+    std::string tf_frame;
+    pnh.param<std::string>("frame_id", tf_frame, std::string("/base_link"));
+    Cloud::Ptr pc_msg(new Cloud);
+    //vector<XYZCoordinate> values;
+    //XYZCoordinate point3d;
+
+    pc_msg->header.frame_id = tf_frame;
+    pc_msg->is_dense = false;
+    pc_msg->width = 16;
+    pc_msg->height = 1;
+    ros::Publisher pub = nh.advertise<Cloud>("profiles", 100);
+    ros::Subscriber posy_sub = nh.subscribe("posy", 100, pos_callback);
     ros::Publisher mid_laser_pub = nh.advertise<std_msgs::Float32>("mid_laser" ,100);
     ros::Rate loop_rate(1000); //1000Hz = 0.05sec
     //ros node & publisher init
@@ -47,43 +71,53 @@ int main(int argc, char**argv)
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr=inet_addr(KEYENCE);
     servaddr.sin_port=htons(PORT);
-    int a = connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));\
+    int connect_check = connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
     //socket init
     
     double init_val = 0;
-    if(a == 0){
-        bool init_flag = true;
-        while(ros::ok()){
-            send(sockfd, LJV_GetMeasurementValues, LJV_GetMeasurementValues_s, MSG_DONTROUTE);
-            char recvline[1000];  //byte-buffer for response
-            int n;
-            usleep(5000); //simply wait x ms for controller to answer - replace this with an event-driven approach if needed
-            n=read(sockfd,recvline,1000); //read socket/received data || ,0,NULL,NULL
-            recvline[n]=0; //add string termination character 
-            int i = 1;
-            for(i =1; i<=16; i++){
-                int msb, lsb, byte2, byte3, outStartBytePosition, outMeasurementValue, byteOffset; //integer-vairables
-                double outMeasurementValueMM; //measurement value in mm-scale should be a float value
-                byteOffset = (i-1) * 8; //every Out has an Offset of 8 byte to the prvious one (except Out1 which starts in byte 232) 
-                outStartBytePosition = 232 + byteOffset; //Out1 measurement value starts with lsb at byte 232 therefore we add OutNumber*8 to 
-                msb = (unsigned char)recvline[outStartBytePosition+3]; //due to network byte order, msb is the last of 4 bytes
-                byte2 =  (unsigned char)recvline[outStartBytePosition+2]; //followed by byte #2
-                byte3 = (unsigned char)recvline[outStartBytePosition+1];  //byte #3
-                lsb = (unsigned char)recvline[outStartBytePosition]; //and lsb
-                outMeasurementValue = msb << 24 | byte2 << 16 | byte3 << 8 | lsb; //shift bytes to big endian
-                outMeasurementValueMM = outMeasurementValue * 0.00001; //since values are stored in 10nm units we have to multiply with factor 0.00001 to reach a mm-scale
-                cloud[i-1].x = 0;
-                cloud[i-1].y = 0;
-                cloud[i-1].z = outMeasurementValueMM;                
-                printf("%d :: Real OUT value = %fmm \n",i, outMeasurementValueMM); //output decimal value for OUT1-16
-                mid_laser.data = outMeasurementValueMM;
-                mid_laser_pub.publish(mid_laser);
-            }
-        //pcl::io::savePCDFileASCII("test_pcd.pcd", cloud);
-        for(const auto& point: cloud)
-            std::cerr <<"    "<<point.x<<"  "<<point.y<<"   "<<point.z << endl;  
-        loop_rate.sleep();
+    if(connect_check == 0){
+    bool init_flag = true;
+        send(sockfd, LJV_GetMeasurementValues, LJV_GetMeasurementValues_s, MSG_DONTROUTE);
+        char recvline[1000];  //byte-buffer for response
+        int n;
+        usleep(5000); //simply wait x ms for controller to answer - replace this with an event-driven approach if needed
+        n=read(sockfd,recvline,1000); //read socket/received data || ,0,NULL,NULL
+        recvline[n]=0; //add string termination character 
+
+        pc_msg->points.clear();
+        double x = 0, y = 0, z = 0;
+        pc_msg->points.reserve(16);
+        for(int i =1; i<=16; i++){
+            int msb, lsb, byte2, byte3, outStartBytePosition, outMeasurementValue, byteOffset; //integer-vairables
+            double outMeasurementValueMM; //measurement value in mm-scale should be a float value
+            byteOffset = (i-1) * 8; //every Out has an Offset of 8 byte to the prvious one (except Out1 which starts in byte 232) 
+            outStartBytePosition = 232 + byteOffset; //Out1 measurement value starts with lsb at byte 232 therefore we add OutNumber*8 to 
+            msb = (unsigned char)recvline[outStartBytePosition+3]; //due to network byte order, msb is the last of 4 bytes
+            byte2 =  (unsigned char)recvline[outStartBytePosition+2]; //followed by byte #2
+            byte3 = (unsigned char)recvline[outStartBytePosition+1];  //byte #3
+            lsb = (unsigned char)recvline[outStartBytePosition]; //and lsb
+            outMeasurementValue = msb << 24 | byte2 << 16 | byte3 << 8 | lsb; //shift bytes to big endian
+            outMeasurementValueMM = outMeasurementValue * 0.00001; //since values are stored in 10nm units we have to multiply with factor 0.00001 to reach a mm-scale
+            x = i*0.001;
+            y = pos_y *0.001;
+            z = -outMeasurementValueMM*0.001;
+            //point3d.x[i-1] = x;
+            //point3d.y[i-1] = y;
+            //point3d.z[i-1] = z;
+                            
+            printf("%d :: Real OUT value = %fmm \n",i, outMeasurementValueMM); //output decimal value for OUT1-16
+            //mid_laser.data = outMeasurementValueMM;
+            //mid_laser_pub.publish(mid_laser);
+            pc_msg->points.push_back(pcl::PointXYZ(x, y, z));
         }
+        pub.publish(pc_msg);
+        /*cout<< "cloud " << pc_msg->points[0] << endl;
+        if(pos_y!=temp){
+            values.push_back(point3d);
+        }
+        cout << "size :" << values.size() << endl;
+        cout << values[0].x << endl;*/
+    ros::spin();
     }
     else{
         printf("not connecting \n ");
