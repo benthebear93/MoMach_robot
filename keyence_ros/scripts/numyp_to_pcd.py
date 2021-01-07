1869c58c426c1f2dd66705820baf197715b1caef
benthebear93 <nswve@naver.com>
2021-01-07 15:29:29 +0900

    pass, static filter added

--- keyence_ros/scripts/numyp_to_pcd.py
+++ keyence_ros/scripts/numyp_to_pcd.py
@@ -18,6 +18,7 @@ def read_pcd():
 	return profile_np, profile_pcd
 
 def visualize(pc_data):
+	print("visualize start")
 	centred = pc_data - np.mean(pc_data, 0)
 	pc_centred = pcl.PointCloud()
 	pc_centred.from_array(centred)
@@ -27,25 +28,9 @@ def visualize(pc_data):
 	v = True
 	while v:
 		v = not(visual.WasStopped())
-		
-def outlier_save():
-	print("outlier save start")
+
+def filter_remove():
 	profile_np, profile_pcd = read_pcd() 
-	# point cloud numpy, pcd read
-
-	fil = profile_pcd.make_statistical_outlier_filter()
-	print("filtered")
-	fil.set_mean_k(10)
-	fil.set_std_dev_mul_thresh(1.0)
-
-	a = fil.filter() 
-	print("filtered type : ", type(a))
-	#pcl.save(fil.filter(),"/home/benlee/Desktop/inliers.pcd")
-	fil.set_negative(True)
-	#pcl.save(fil.filter(),"/home/benlee/Desktop/outliers.pcd")
-	b = fil.filter()
-	print("filtered type : ", type(b))
-	#statistical outlier filter result save
 
 	num_point = len(profile_np) #number of points
 	final = []
@@ -57,8 +42,40 @@ def outlier_save():
 		profile_np[i] = changed_arr 
 	#infinity value check
 	final = np.array(final, dtype=np.float32) #list to numpy
+
+	# point cloud numpy, pcd read
+	ori_cloud = pcl.PointCloud(final) #numpy to pcl
+
+	pcl.save(ori_cloud,"/home/benlee/Desktop/ori_cloud.pcd")
+	print("before :", ori_cloud.size)
+	print("passthrough filter start")
+	passthrough = ori_cloud.make_passthrough_filter()
+	passthrough.set_filter_field_name("z")
+	passthrough.set_filter_limits(-35, -10)
+	pass_filtered = passthrough.filter()
+	pcl.save(pass_filtered,"/home/benlee/Desktop/pass_filtered.pcd")
+	print("after :", pass_filtered.size)
+
+	print("outlier save start")
+	fil = profile_pcd.make_statistical_outlier_filter()
+	print("filtered")
+	fil.set_mean_k(50)
+	fil.set_std_dev_mul_thresh(1.0)
+
+	inliter_filtered = fil.filter() 
+	pcl.save(inliter_filtered,"/home/benlee/Desktop/50_inliers.pcd")
+	fil.set_negative(True)
+	outlier_filtered = fil.filter()
+	pcl.save(outlier_filtered,"/home/benlee/Desktop/50_ioutliers.pcd")
+
+	#visualize(cloud_filtered)
+
+	#statistical outlier filter result save
+
+	v = pptk.viewer(final)
+	v.set(point_size=0.01)
+ 
 	pc = pcl.PointCloud(final) #numpy to pcl
-	visualize(pc)
 
 	cloud_new = pcl_helper.pcl_to_ros(pc)
 
@@ -70,5 +87,5 @@ def outlier_save():
 if __name__=="__main__":
 	rospy.init_node('cmm_test', anonymous=True)
 	pub = rospy.Publisher("cmm_profile", PointCloud2, queue_size=1)
-	outlier_save()
+	filter_remove()
 	rospy.spin()
