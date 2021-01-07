44ea32e695b52d25803cd033705c79a4f023e3a2
benthebear93 <nswve@naver.com>
2021-01-07 12:01:56 +0900

    visualize function added

--- keyence_ros/scripts/numyp_to_pcd.py
+++ keyence_ros/scripts/numyp_to_pcd.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
-
 import pcl
 import pcl_helper
+import pcl.pcl_visualization
 import numpy as np
 import pptk
 
@@ -11,43 +11,64 @@ import sensor_msgs.point_cloud2 as pc2
 
 def read_pcd():
 	print("read pcd start")
-	profile_load = np.load('/home/benlee/Desktop/pptk_save.npy')
+	profile_np = np.load('/home/benlee/Desktop/pptk_save.npy')
+	print("load type :", type(profile_np))
 	profile_pcd = pcl.load("/home/benlee/Desktop/pocket.pcd")
 
+	return profile_np, profile_pcd
+
+def visualize(pc_data):
+	centred = pc_data - np.mean(pc_data, 0)
+	pc_centred = pcl.PointCloud()
+	pc_centred.from_array(centred)
+
+	visual = pcl.pcl_visualization.CloudViewing()
+	visual.ShowMonochromeCloud(pc_centred,b'pc_data')
+	v = True
+	while v:
+		v = not(visual.WasStopped())
+		
+def outlier_save():
+	print("outlier save start")
+	profile_np, profile_pcd = read_pcd() 
+	# point cloud numpy, pcd read
+
 	fil = profile_pcd.make_statistical_outlier_filter()
+	print("filtered")
 	fil.set_mean_k(10)
 	fil.set_std_dev_mul_thresh(1.0)
 
-	pcl.save(fil.filter(),
-				"/home/benlee/Desktop/inliers.pcd")
-
+	a = fil.filter() 
+	print("filtered type : ", type(a))
+	#pcl.save(fil.filter(),"/home/benlee/Desktop/inliers.pcd")
 	fil.set_negative(True)
-	pcl.save(fil.filter(),
-				"/home/benlee/Desktop/outliers.pcd")
+	#pcl.save(fil.filter(),"/home/benlee/Desktop/outliers.pcd")
+	b = fil.filter()
+	print("filtered type : ", type(b))
+	#statistical outlier filter result save
 
-	print(profile_load)
+	num_point = len(profile_np) #number of points
 	final = []
-	for i in range(0, (929600)):
-		check =profile_load[i]
-		#print(check)
+	for i in range(0, num_point):
+		check =profile_np[i]
 		changed_arr = np.where(check == np.inf, 0, check)
-		final.append(list(changed_arr))
-		profile_load[i] = changed_arr
-	final = np.array(final, dtype=np.float32)
 
-	pc = pcl.PointCloud(final)
+		final.append(list(changed_arr))
+		profile_np[i] = changed_arr 
+	#infinity value check
+	final = np.array(final, dtype=np.float32) #list to numpy
+	pc = pcl.PointCloud(final) #numpy to pcl
+	visualize(pc)
+
 	cloud_new = pcl_helper.pcl_to_ros(pc)
-	#cloud_new = pcl_helper.XYZRGB_to_XYZ(cloud_new)
+
+	#pointcloud XYZ to ROS PointCloud2 msg
 	pub.publish(cloud_new)
 
-	#print("pcl.pointcloud form array", pc, type(pc))
-	#print(pc)
+	
 
 if __name__=="__main__":
 	rospy.init_node('cmm_test', anonymous=True)
 	pub = rospy.Publisher("cmm_profile", PointCloud2, queue_size=1)
-	read_pcd()
+	outlier_save()
 	rospy.spin()
-#visualize 
-#v = pptk.viewer(final)
-#v.set(point_size = 0.00001)
