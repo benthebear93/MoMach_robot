97bd3bb9ec3b1bec5f190fc46bc90634992829a7
benthebear93 <nswve@naver.com>
2021-01-14 21:16:04 +0900

    normal vector example file

--- keyence_ros/scripts/normal_vector.py
+++ keyence_ros/scripts/normal_vector.py
@@ -1,11 +1,22 @@
-import numpy as np
-import scipy.linalg
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-import numpy as np
 from sklearn.decomposition import PCA
+from matplotlib.patches import FancyArrowPatch
+from mpl_toolkits.mplot3d import proj3d
+#library imports
+import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from scipy.spatial import KDTree
+
+class Arrow3D(FancyArrowPatch):
+    def __init__(self, xs, ys, zs, *args, **kwargs):
+        FancyArrowPatch.__init__(self, (0,0), (0,0), *args, **kwargs)
+        self._verts3d = xs, ys, zs
+
+    def draw(self, renderer):
+        xs3d, ys3d, zs3d = self._verts3d
+        xs, ys, zs = proj3d.proj_transform(xs3d, ys3d, zs3d, renderer.M)
+        self.set_positions((xs[0],ys[0]),(xs[1],ys[1]))
+        FancyArrowPatch.draw(self, renderer)
+
 
 X = np.array([[24,13,38],[8,3,17],[21,6,40],[1,14,-9],[9,3,21],[7,1,14],[8,7,11],[10,16,3],[1,3,2],
     [15,2,30],[4,6,1],[12,10,18],[1,9,-4],[7,3,19],[5,1,13],[1,12,-6],[21,9,34],[8,8,7],
@@ -15,13 +26,13 @@ pca = PCA(n_components=3)
 pca.fit(X)
 eig_vec = pca.components_
 
-print(pca.explained_variance_ratio_)
 # [0.90946569 0.08816839 0.00236591]
 # Percentage of variance explain by last vector is less 0.2%
 
 # This is the normal vector of minimum variance
 normal = eig_vec[2, :]  # (a, b, c)
-print("normal :", normal)
+normal2 = eig_vec[1, :]
+normal3 = eig_vec[0, :]
 centroid = np.mean(X, axis=0)
 
 # Every point (x, y, z) on the plane should satisfy a*x+b*y+c*z = d
@@ -33,51 +44,32 @@ xx, yy = np.meshgrid(np.arange(np.min(X[:, 0]), np.max(X[:, 0])), np.arange(np.m
 
 z = (-normal[0] * xx - normal[1] * yy - d) * 1. / normal[2]
 
+
+kdtree=KDTree(X)
+dist, points=kdtree.query(X, 10)
+print("kd_tree dist : ", dist)
+print("kd_tree points : ", points)
+mean_x = np.mean(xx)
+mean_y = np.mean(yy)
+mean_z = np.mean(z)
 # plot the surface
 plt3d = plt.figure().gca(projection='3d')
-plt3d.plot_surface(xx, yy, z)
-plt3d.scatter(*(X.T))
+plt3d.plot_surface(xx, yy, z, alpha=0.2)
+plt3d.scatter(mean_x, mean_y, mean_z, edgecolor="b", marker='s')
+a = Arrow3D([mean_x, normal[0]], [mean_y, normal[1]], [mean_z, normal[2]], mutation_scale=20, lw=3, arrowstyle="-|>", color="r")
+b = Arrow3D([mean_x, normal2[0]], [mean_y, normal2[1]], [mean_z, normal2[2]], mutation_scale=20, lw=3, arrowstyle="-|>", color="r")
+c = Arrow3D([mean_x, normal3[0]], [mean_y, normal3[1]], [mean_z, normal3[2]], mutation_scale=20, lw=3, arrowstyle="-|>", color="r")
+a_ = [[mean_x, normal[0]], [mean_y, normal[1]], [mean_z, normal[2]]]
+b_ = [[mean_x, normal2[0]], [mean_y, normal2[1]], [mean_z, normal2[2]]]
+c_ = [[mean_x, normal3[0]], [mean_y, normal3[1]], [mean_z, normal3[2]]]
+n_vector = np.cross(a_, b_)
+n_vector_ = Arrow3D([mean_x, n_vector[0]], [mean_y, n_vector[1]], [mean_z, n_vector[2]], mutation_scale=20, lw=3, arrowstyle="-|>", color="b")
+plt3d.add_artist(a)
+plt3d.add_artist(b)
+plt3d.add_artist(c)
+plt3d.add_artist(n_vector_)
+print("*xt", *(X.T))
+print("points", points[0])
+plt3d.scatter(*(X.T), edgecolors="r")
+plt3d.scatter(points[0], edgecolors="g")
 plt.show()
-'''
-# some 3-dim points
-mean = np.array([0.0, 0.0, 0.0])
-cov = np.array([[1.0, -0.5, 0.8], [-0.5, 1.1, 0.0], [0.8, 0.0, 1.0]])
-data = np.random.multivariate_normal(mean, cov, 50)
-
-# regular grid covering the domain of the data
-X, Y = np.meshgrid(np.arange(-3.0, 3.0, 0.5), np.arange(-3.0, 3.0, 0.5))
-XX = X.flatten()
-YY = Y.flatten()
-
-order = 1  # 1: linear, 2: quadratic
-if order == 1:
-    # best-fit linear plane
-    A = np.c_[data[:, 0], data[:, 1], np.ones(data.shape[0])]
-    C, _, _, _ = scipy.linalg.lstsq(A, data[:, 2])  # coefficients
-
-    # evaluate it on grid
-    Z = C[0] * X + C[1] * Y + C[2]
-
-    # or expressed using matrix/vector product
-    # Z = np.dot(np.c_[XX, YY, np.ones(XX.shape)], C).reshape(X.shape)
-
-elif order == 2:
-    # best-fit quadratic curve
-    A = np.c_[np.ones(data.shape[0]), data[:, :2], np.prod(data[:, :2], axis=1), data[:, :2] ** 2]
-    C, _, _, _ = scipy.linalg.lstsq(A, data[:, 2])
-
-    # evaluate it on a grid
-    Z = np.dot(np.c_[np.ones(XX.shape), XX, YY, XX * YY, XX ** 2, YY ** 2], C).reshape(X.shape)
-
-# plot points and fitted surface
-fig = plt.figure()
-ax = fig.gca(projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, alpha=0.2)
-ax.scatter(data[:, 0], data[:, 1], data[:, 2], c='r', s=50)
-
-plt.xlabel('X')
-plt.ylabel('Y')
-ax.set_zlabel('Z')
-#ax.axis('equal')
-#ax.axis('tight')
-plt.show()'''
