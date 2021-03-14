eb4089f9e0e7fe56feac37aed0a00fe5ca60b561
benthebear93 <nswve@naver.com>
2021-03-14 17:09:10 +0900

    system model update

--- simulation/vertical_movement.m
+++ simulation/vertical_movement.m
@@ -7,66 +7,66 @@ F = [1 0 0;
     0 0 0;
     0 0 0];
 
-% w = (2*pi*u)/one_rotation_dis;
-
-% B = [1*u; offset*sin(w*time) + roller*sin(9*w*time); u];
-
 H = [1 0 0; 0 1 0; 0 0 1];
 
 offset = -0.15;
-roller = 0.08;
+roller = 0.08; % Wheel axis offset and roller error
+u = 0.833; % feedrate
+T = 1;
+one_rotation_dis = 314.159;
+w = (2*pi*u)/one_rotation_dis;
 %% Initial Conditions
-xTrue(:,1) = [0;0;0]; 
-xEst(:,1) = [0;0;0]; 
-xPred = [0;0;0]; 
+xTrue(:,1) = [0;0;0]; % true initial state
+xEst(:,1) = [0;-0.3;0]; % estimation state
+xPred = [0;0;0]; %initial posteriori estimation
 nx = length(xPred(:,1)); % number of state
 Pp = eye(nx); % guess of initial error covariance
 sigma_Pp(:,1) = sqrt(diag(Pp));
-z(:,1) = [0; 0; 0];
+
+z(:,1) = [0; 0; 0]; 
 zPred(:,1) =[0; 0; 0];
-sigma_w = [0.1 0.1 0.1]';
-sigma_v = [0, 0.3, 0.3]';
+
+%% Noise
+sigma_w = [0.1 0.1 0.1]'; % system noise (std of x, z, vel)
+sigma_v = [0, 0.3, 0.3]'; % measurement noise 
 Q = diag(sigma_w.^2);
 R = diag(sigma_v.^2);
 
-T = 1;
 t = 0:T:400;
-u = 0.833;
+%% KF Routine
 for i = 1:length(t)-1
-    one_rotation_dis = 314.159;
-    w = (2*pi*u)/one_rotation_dis;
+    %% time varying system
     B = [1*u; offset*sin(w*i) + roller*sin(9*w*i); u];
-    xTrue(:,i+1) = F*xTrue(:,i) + B;
-    %ground truth
-    
-    ud = u + Q * randn(1,1);
-    z(:,i+1) = H*xTrue(:,i);
-    % observation
-    
-    B = [1*u; offset*sin(w*i) + roller*sin(9*w*i); u];
+    %% True dynamics
+    xTrue(:,i+1) = F*xTrue(:,i) + B + sigma_w;
+    %% Time update
+    %% Prediction of state
     xPred = F*xEst(:,i)+B;
     jF = jaco_f(xEst(:,i), u, i, offset, roller);
+    %% Prediction of covariance
     PPred = jF*Pp*jF'+Q
-    % predict
+    %% Measurement generation
+    %% input noise and measurement update
+    ud = u + Q * randn(1,1);
+    z(:,i+1) = H*xTrue(:,i);
+    %% Compute jacobian
     jH = jaco_h()
-    zPred(:,i+1) = observation_model(xPred);
+    zPred(:,i+1) = observation_model(xPred)+ sigma_v;
     y = z(:,i+1)-zPred(:,i+1);
+    %% Innovation Covariance
     S = jH*PPred*jH'+R;
+    %% Kalman gain
     K = PPred*jH'*inv(S);
-    xPred
-    K
-    y
+    %% State update
     xEst(:,i+1) = xPred + K*y
-    Pp = eye(length(xEst(:,i+1))-K*jH)*PPred
-    
-%     obs_val= observation(xTrue(:,i), u, i, offset, roller);
-%     xTrue(:,i+1) = obs_val(1); z(:,i+1) = obs_val(2);
-%     ud = u + input_noise * randn(1,1)
-%     est_val = ekf_estimation(xEst(:,i), Pp, z(:,i), ud, i, offset, roller);
-%     xEst(:,i+1) = est_val(1); Pp(:,i+1) = est_val(2);
+    %% Covariance update
+    length(xEst(:,i+1))
+    Pp = (eye(length(xEst(:,i+1)))-K*jH)*PPred
 end
 figure; clf; hold on;
-plot(xTrue(1,:),xTrue(2,:),'.-b');        
+plot(xTrue(1,:),xTrue(2,:),'.-b');    
+plot(xEst(1,:),xEst(2,:),'.-r');    
+plot(zPred(1,:),zPred(2,:),'.-g');    
 xlabel('x, m'); ylabel('z, m')
 end
 %% Plot: position estimate x_1
@@ -87,14 +87,6 @@ H = [1 0 0; 0 1 0; 0 0 1];
 z = H*x;
 end
 
-function obs_val= observation(xTrue, u, time, offset, roller)
-disp("here");
-xTrue = motion_model(xTrue, u, time, offset, roller);
-ud = u + input_noise * randn(1,1)
-z = observation_model(xTrue);
-obs_val =[xTrue z];
-end
-
 function jf = jaco_f(x, u, time, offset, roller)
 one_rotation_dis = 314.159;
 u = x(3);
@@ -108,19 +100,4 @@ function jh = jaco_h()
 jh = [ 1, 0, 0;
     0 1 0;
     0 0 1];
-end
-
-function est_val = ekf_estimate(xEst, PEst, z, u, time, offset, roller, Q, R)
-xPred = motion_model(xEst, u, time, offset, roller);
-jf = jaco_f(xEst, u, time, offset, roller);
-PPred = jf*PEst*jf'+Q ;
-
-jh = jaco_h();
-zPred = observation_model(xPred);
-y = z - zPred;
-S = jh*PPred*jh' + R;
-K = PPred * jh' * inv(S);
-xEst = xPred + K*y;
-PEst = (eye(3) - K*jh)*PPred;
-est_val = [xEst PEst];
 end
