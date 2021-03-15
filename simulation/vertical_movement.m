8fcc50f99be78b09c6c0509c1d92eb3cc69d40a5
benthebear93 <nswve@naver.com>
2021-03-15 17:38:52 +0900

    tool and specimen visualization

--- simulation/vertical_movement.m
+++ simulation/vertical_movement.m
@@ -1,23 +1,26 @@
 function main()
-%% Extended Kalman Filter Simulation
 clear all; close all;
 clc
+%% Extended Kalman Filter Simulation
 %% system model
 F = [1 0 0;
     0 0 0;
     0 0 0];
 
-H = [1 0 0; 0 1 0; 0 0 1];
+H = [1 0 0; 
+    0 1 0; 
+    0 0 1];
 
 offset = -0.15;
 roller = 0.08; % Wheel axis offset and roller error
+% u =input ("set the feedrate (mm/min)")
 u = 0.833; % feedrate
 T = 1;
 one_rotation_dis = 314.159;
 w = (2*pi*u)/one_rotation_dis;
 %% Initial Conditions
 xTrue(:,1) = [0;0;0]; % true initial state
-xEst(:,1) = [0;-0.3;0]; % estimation state
+xEst(:,1) = [0;0;0]; % estimation state
 xPred = [0;0;0]; %initial posteriori estimation
 nx = length(xPred(:,1)); % number of state
 Pp = eye(nx); % guess of initial error covariance
@@ -31,43 +34,74 @@ sigma_w = [0.1 0.1 0.1]'; % system noise (std of x, z, vel)
 sigma_v = [0, 0.3, 0.3]'; % measurement noise 
 Q = diag(sigma_w.^2);
 R = diag(sigma_v.^2);
-
+measurement_noise = diag([0,0.1,0.1]).^2;
+input_noise = diag([0.01]).^2;
 t = 0:T:400;
 %% KF Routine
-for i = 1:length(t)-1
-    %% time varying system
-    B = [1*u; offset*sin(w*i) + roller*sin(9*w*i); u];
-    %% True dynamics
-    xTrue(:,i+1) = F*xTrue(:,i) + B + sigma_w;
-    %% Time update
-    %% Prediction of state
-    xPred = F*xEst(:,i)+B;
-    jF = jaco_f(xEst(:,i), u, i, offset, roller);
-    %% Prediction of covariance
-    PPred = jF*Pp*jF'+Q
-    %% Measurement generation
-    %% input noise and measurement update
-    ud = u + Q * randn(1,1);
-    z(:,i+1) = H*xTrue(:,i);
-    %% Compute jacobian
-    jH = jaco_h()
-    zPred(:,i+1) = observation_model(xPred)+ sigma_v;
-    y = z(:,i+1)-zPred(:,i+1);
-    %% Innovation Covariance
-    S = jH*PPred*jH'+R;
-    %% Kalman gain
-    K = PPred*jH'*inv(S);
-    %% State update
-    xEst(:,i+1) = xPred + K*y
-    %% Covariance update
-    length(xEst(:,i+1))
-    Pp = (eye(length(xEst(:,i+1)))-K*jH)*PPred
-end
-figure; clf; hold on;
-plot(xTrue(1,:),xTrue(2,:),'.-b');    
-plot(xEst(1,:),xEst(2,:),'.-r');    
-plot(zPred(1,:),zPred(2,:),'.-g');    
-xlabel('x, m'); ylabel('z, m')
+grid on
+hold on
+spicemen_x = [0 400 400 0];
+spicemen_y = [0 0 1 1];
+tool_x = [-1.5 1.5 1.5 -1.5];
+tool_y = [0.5 0.5 1.5 1.5];
+axis([0 400 0 5]);
+box =fill(spicemen_x, spicemen_y,'Black');
+tool =fill(tool_x, tool_y,'White');
+%remove_area =fill(tool_x, tool_y,'b','LineStyle','none'); %,'EraseMode','none'
+    for i = 1:length(t)-1
+        offset_e = -0.15 + 0+0.05*rand(1,1);
+        roller_e = 0.08-0.01+0.01*rand(1,1); % Wheel axis offset and roller error
+        %% time varying system
+        B = [1*u; offset*sin(w*i) + roller*sin(9*w*i); u];
+        %% True dynamics (observation)
+        xTrue(:,i+1) = F*xTrue(:,i) + B;
+            z(:,i+1) = H*xTrue(:,i) + measurement_noise *randn(3,1);
+            ud = u + input_noise * randn(1,1);
+            w = (2*pi*u)/one_rotation_dis;
+            Bd = [1*ud; offset_e*sin(w*i) + roller_e*sin(9*w*i); ud];
+        %% Time update
+        %% (ekf estimation) (xEst, PEst, z, ud, time, offset, roller)
+        %% Prediction of state
+        xPred = F*xEst(:,i)+Bd;
+        jF = jaco_f(xEst(:,i), ud, i, offset_e, roller_e);
+        %% Prediction of covariance
+        Pp = jF*Pp*jF'+Q;
+        jH = jaco_h();
+        %% Measurement generation
+        %% input noise and measurement update
+        %% Compute jacobian
+        zPred(:,i+1) = observation_model(xPred); %+ sigma_v;
+        y = z(:,i+1)-zPred(:,i+1);
+        %% Innovation Covariance
+        S = jH*Pp*jH'+R;
+        %% Kalman gain
+        K = Pp*jH'*inv(S);
+        %% State update
+        xEst(:,i+1) = xPred + K*y;
+        %% Covariance update
+        Pp = (eye(length(xEst(:,i+1)))-K*jH)*Pp;
+        x_t = xEst(1,:);
+        z_t = xEst(2,:);
+        if i >1
+            tempX = [(x_t(i-1)-1.5) (x_t(i-1)+1.5) (x_t(i-1)+1.5) (x_t(i-1)-1.5)];
+            tempY = [0.75+z_t(i-1) 0.75+z_t(i-1) z_t(i-1)+1.5 z_t(i-1)+1.5];
+            remove_area =fill(tempX, tempY,'White','LineStyle','none'); %,'EraseMode','none'
+            set(remove_area,'Xdata', tempX, 'Ydata', tempY);
+        end
+        tool_x = [(x_t(i)-1.5) (x_t(i)+1.5) (x_t(i)+1.5) (x_t(i)-1.5)];
+        tool_z = [0.75+z_t(i) 0.75+z_t(i) z_t(i)+1.5 z_t(i)+1.5];
+
+        updateX = tool_x;
+        updateY = tool_z;
+        set(tool,'Xdata', updateX, 'Ydata', updateY);
+        drawnow;
+        plot(xTrue(1,:)-1.5,0.75,'.-g'); 
+        plot(xTrue(1,:),0.75+xTrue(2,:),'.-b');    
+        plot(xEst(1,:),0.75+xEst(2,:),'.-r');
+%         plot(z(1,:),0.75+z(2,:),'.-y');
+    %     pause(0.2)
+    %     end
+    end
 end
 %% Plot: position estimate x_1
 
@@ -89,10 +123,10 @@ end
 
 function jf = jaco_f(x, u, time, offset, roller)
 one_rotation_dis = 314.159;
-u = x(3);
+v = x(3);
 w = 2*pi*u/one_rotation_dis;
 jf = [ 1.0, 0.0, 1;
-    0 0 (2*pi*time/one_rotation_dis)*cos(w*time)+(u*roller/9/offset)*cos(u/9/offset);
+    0 0 (2*pi*time/one_rotation_dis)*cos(w*time)+(v*roller/9/offset)*cos(v*i/9/offset);
     0 0 1];
 end
 
